fa6b82ddedf2087bb29db69f7af5abf3b6b34369
gabvask <gabrielvascoauzier@gmail.com>
2022-05-06 00:09:42 -0400

    feat: add feedback post

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { routes } from "./routes";
+import cors from "cors";
+
+const app = express();
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET = Buscar informações
+// POST = Cadastrar informações
+// PUT = Atualizar informações de uma entidade
+// PATCH = Atualizar uma informação única de uma entidade
+// DELETE = Deletar uma informação
+
+app.use(cors());
+app.use(express.json()); // plug-in
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log("HTTP server running");
+});
